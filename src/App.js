643feecb26917cf617f691db77877906dc6bd8cd
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2021-02-26 11:43:14 +0200

    changed file App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
+import node_logo from './node_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src={node_logo} className="Node-logo" alt="logo" />
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Nodejs + React app
+        </h1>
       </header>
     </div>
   );
